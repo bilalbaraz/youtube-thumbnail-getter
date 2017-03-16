216d62a4846a87917d8510f5197d130caaaa5d80
Bilal Baraz <bilal@bilalbaraz.com>
2017-03-17 01:28:47 +0200

    project structure was added

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Content extends Component {
+    render() {
+        return (
+            <main>
+                <h1>Content</h1>
+            </main>
+        );
+    }
+}
